f3cd04fa72dfc80e14f494a115d93aa6d582d6cf
Fael3113 <111023863+Fael3113@users.noreply.github.com>
2023-04-23 13:55:44 -0300

    Update Exemplo01.java

--- src/aula0419/Exemplo01.java
+++ src/aula0419/Exemplo01.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exemplo01 {
     
-    public static void limparTabuleiro(char[][] tabuleiro){
+public static void limparTabuleiro(char[][] tabuleiro){
         for(int i = 0; i < tabuleiro.length; i++){
             for(int j = 0; j < tabuleiro.length; j++){
                 tabuleiro[i][j] = '-';
@@ -27,22 +27,56 @@ public class Exemplo01 {
         } else {
             return false;
         }
-    }
-            
-    public static void main(String[] args) {
-        char[][] tabuleiro = new char[3][3];
-        limparTabuleiro(tabuleiro);
-        mostrarTabuleiro(tabuleiro);
+    }  
+    
+    public static void jogador1(char[][] tabuleiro) {
         var tc = new Scanner(System.in);
         int linha;
         int coluna;
         do {
+            System.out.println("Primeiro Jogador");
             System.out.println("Digite a posição linha (de 0 a 2)");
             linha = tc.nextInt();
             System.out.println("Digite a posição ooluna (de 0 a 2)");
             coluna = tc.nextInt();
+            System.out.println("Sua rodada terminou");
         } while (!testaJogada(tabuleiro, linha, coluna));
         tabuleiro[linha][coluna] = 'x';
         mostrarTabuleiro(tabuleiro);
     }
+    
+    public static void jogador2(char [][] tabuleiro) {
+        var tc = new Scanner(System.in);
+        int linha;
+        int coluna;
+        do {
+            System.out.println("Segundo jogador");
+            System.out.println("Digite a posição linha (de 0 a 2)");
+            linha = tc.nextInt();
+            System.out.println("Digite a posição ooluna (de 0 a 2)");
+            coluna = tc.nextInt();
+            System.out.println("Sua rodada terminou");
+        } while (!testaJogada(tabuleiro, linha, coluna));
+        tabuleiro[linha][coluna] = 'o';
+        mostrarTabuleiro(tabuleiro);
+    }
+    
+    public static boolean tabuleiroCompleto(char[][] tabuleiro){
+        for(int linha=0 ; linha<3 ; linha++)
+            for(int coluna=0 ; coluna<3 ; coluna++)
+                if(tabuleiro[linha][coluna] == 0)
+                    return false;
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        char[][] tabuleiro = new char[3][3];
+        limparTabuleiro(tabuleiro);
+        mostrarTabuleiro(tabuleiro);
+        do{
+        jogador1(tabuleiro);
+        jogador2(tabuleiro);
+        } while (tabuleiroCompleto(tabuleiro));
+    }
+
 }

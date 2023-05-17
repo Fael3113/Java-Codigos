a88cbd96d9ee7b77319941ce5e47c57164a61669
Fael3113 <111023863+Fael3113@users.noreply.github.com>
2023-05-17 11:35:18 -0300

    Update Main.java

--- src/aula0426/Main.java
+++ src/aula0426/Main.java
@@ -1,5 +1,11 @@
 package aula0426;
 
 public class Main {
-    
+    Nota aluno = new Nota();
+        
+    aluno.setNota(6.3, 0);
+    aluno.setNota(7.4, 1);
+    aluno.setNota(8.5 , 2);
+    System.out.printf("Media = %.2f\n", aluno.calcMedia());
+    aluno.print();
 }

b07edb4492f07592e4a43e7fb951c534be8c8c75
Fael3113 <santiagorafaelsantiago@hotmail.com>
2023-05-22 09:01:02 -0300

    Update Main.java

--- src/aula0426/Main.java
+++ src/aula0426/Main.java
@@ -1,11 +1,6 @@
 package aula0426;
 
 public class Main {
-    Nota aluno = new Nota();
-        
-    aluno.setNota(6.3, 0);
-    aluno.setNota(7.4, 1);
-    aluno.setNota(8.5 , 2);
-    System.out.printf("Media = %.2f\n", aluno.calcMedia());
-    aluno.print();
+    AlunoPSC aluno = new AlunoPSC("Pablo", 123412);
+    
 }

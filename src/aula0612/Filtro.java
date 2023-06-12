cf6fd82a4acc9f1c48a5bef8319fe42ddb8b761f
Fael3113 <santiagorafaelsantiago@hotmail.com>
2023-06-12 10:21:14 -0300

    main

---
+++ src/aula0612/Filtro.java
@@ -0,0 +1,13 @@
+package aula0612;
+
+import java.io.File;
+
+public class Filtro extends javax.swing.filechooser.FileFilter{
+    public boolean  accept(File file){
+        return file.isDirectory() || file.getAbsolutePath().endsWith(".txt");
+    }
+    
+    public String getDescription() {
+        return "Documento texto (*.txt)";
+    }
+}
